22f2c3d30e3263088b977f115bd1ff2c4602dad4
angad2212 <angadsuryavanshi22@gmail.com>
2025-10-02 22:13:52 +0530

    added second server

---
+++ node-servers/server-2.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+// Unique server ID (so we know which one handled the request)
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send(`Hello from Server running on port ${PORT}`);
+});
+
+app.post("/data", (req, res) => {
+  const { name } = req.body;
+  res.send(`Hello ${name}, response from server on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
